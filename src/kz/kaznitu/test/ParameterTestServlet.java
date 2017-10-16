08a36012ea8d8cf728b460d76cf8f860b4abcb22
Темирлан <tim4kz@gmail.com>
2017-10-16 18:05:04 +0600

    hello.html ди POST методына озгерттим

--- src/kz/kaznitu/test/ParameterTestServlet.java
+++ src/kz/kaznitu/test/ParameterTestServlet.java
@@ -32,5 +32,32 @@ public class ParameterTestServlet extends HttpServlet{
         );
 
     }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        resp.setContentType("text/html");
+
+        PrintWriter out=resp.getWriter();
+        String title = "Using GET Method to Read Form Data";
+        String docType= "<!doctype html>\n";
+
+        out.println(docType +
+                "<html>\n" +
+                "<head><title>" + "</title></head>\n" +
+                "<body bgcolor = \"#f0f0f0\">\n" +
+                "<h1 align = \"center\">" + title + "</h1>\n" +
+                "<ul>\n" +
+                " <li><b>First Name</b>: "
+                + req.getParameter("first_name") + "\n" +
+                " <li><b>Last Name</b>: "
+                + req.getParameter("last_name") + "\n" +
+                "</ul>\n" +
+                "</body>\n" +
+                "</html>"
+        );
+
+    }
 }
+
 
