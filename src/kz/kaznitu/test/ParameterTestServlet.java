d313001313b91ab7ee5f3bf12b3435ebe508ff93
Темирлан <tim4kz@gmail.com>
2017-10-16 17:42:38 +0600

    Initial commit

---
+++ src/kz/kaznitu/test/ParameterTestServlet.java
@@ -0,0 +1,36 @@
+package kz.kaznitu.test;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class ParameterTestServlet extends HttpServlet{
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+
+        PrintWriter out=response.getWriter();
+        String title = "Using GET Method to Read Form Data";
+        String docType= "<!doctype html>\n";
+
+        out.println(docType +
+                "<html>\n" +
+                "<head><title>" + "</title></head>\n" +
+                "<body bgcolor = \"#f0f0f0\">\n" +
+                "<h1 align = \"center\">" + title + "</h1>\n" +
+                "<ul>\n" +
+                " <li><b>First Name</b>: "
+                + request.getParameter("first_name") + "\n" +
+                " <li><b>Last Name</b>: "
+                + request.getParameter("last_name") + "\n" +
+                "</ul>\n" +
+                "</body>\n" +
+                "</html>"
+        );
+
+    }
+}
+
